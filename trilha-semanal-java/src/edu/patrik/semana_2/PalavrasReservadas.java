2c0b325fe6997f6ef1e166ea533042552cecfa3b
PatrikBressan <patrikbressan@gmail.com>
2023-09-21 15:17:46 -0400

    Escopo e Palavras Reservadas em Java.

---
+++ trilha-semanal-java/src/edu/patrik/semana_2/PalavrasReservadas.java
@@ -0,0 +1,17 @@
+package edu.patrik.semana_2;
+
+public class PalavrasReservadas {
+    /*
+     * import: importa pacotes/classes.
+     * package: especifica a que pacote todas as classes de um arquivo pertencem.
+     */
+
+     /* Modificadores de acesso
+      * public
+      private
+      protected
+      */
+      public static void main(String[] args) {
+        System.out.println("Entendendo escopo e palavras reservadas em Java");
+      }
+}
