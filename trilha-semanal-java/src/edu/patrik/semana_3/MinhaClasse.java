416b62e5a1c6b533c919490ec13e91ffe8cdac74
PatrikBressan <patrikbressan@gmail.com>
2023-09-25 14:15:50 -0400

    Argumentos, métodos e scanner.

---
+++ trilha-semanal-java/src/edu/patrik/semana_3/MinhaClasse.java
@@ -0,0 +1,7 @@
+package edu.patrik.semana_3;
+
+public class MinhaClasse {
+    public static void main(String[] args) {
+        System.out.println("Olá, fui executado pelo terminal!");
+    }
+}
---
+++ trilha-semanal-java/src/edu/patrik/semana_3/TerminalArgumentos.java
@@ -0,0 +1,7 @@
+package edu.patrik.semana_3;
+
+public class TerminalArgumentos {
+    public static void main(String[] args) {
+        System.out.println("Testando.");
+    }
+}
